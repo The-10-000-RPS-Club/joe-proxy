903b44b562f39ed9e482acc84e3242554b33ae63
Joe Redmon <joesredmon@gmail.com>
2020-10-28 20:45:25 -0700

    working on proxy

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
 const app = express();
-const PORT = 3005;
+const PORT = 3008;
 app.use(express.static(PUBLIC_DIR));
 
 app.use((req, res, next) => {
@@ -13,10 +13,10 @@ app.use((req, res, next) => {
 });
 
 app.get('/reviews/:id', (req, res) => {
-  axios.get(`http://ec2...`)
+  axios.get(`http://ec2-54-244-0-175.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then((data) => res.send(data))
   .catch((err) => res.send(err));
-}
+});
 
 app.listen(PORT, () => {
   console.log('listening on port: ', PORT);
