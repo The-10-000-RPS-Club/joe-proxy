3a014229974451bb7fcbf4b0822dee4c4c993384
Joe Redmon <joesredmon@gmail.com>
2020-10-29 18:32:32 -0700

    still working on proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
 const app = express();
@@ -8,9 +10,40 @@ const PORT = 3008;
 
 app.use('/products/:id', express.static(PUBLIC_DIR));
 app.use(express.static(PUBLIC_DIR));
+app.use(morgan('dev'));
 
-app.get('/reviews/:id', (req, res) => {
-  axios.get(`http://54.244.0.175:80${req.url}`)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/api/reviews/:id/:count/:sort', (req, res) => {
+  console.log(req.url);
+  axios.get(`http://54.244.0.175${req.url}`)
+  .then((data) => res.send(data))
+  .catch((err) => res.send(err));
+});
+
+app.get('/api/reviews/avg/:id', (req, res) => {
+  axios.get(`http://54.244.0.175${req.url}`)
+  .then((data) => res.send(data))
+  .catch((err) => res.send(err));
+});
+
+app.get('/api/relatedItems/:product_id', (req, res) => {
+  axios.get(`http://34.208.186.96${req.url}`)
+  .then((data) => res.send(data))
+  .catch((err) => res.send(err));
+});
+
+app.get('/api/products/:id', (req, res) => {
+  axios.get(`http://52.10.142.6${req.url}`)
+  .then((data) => res.send(data))
+  .catch((err) => res.send(err));
+});
+
+app.get('/api/questions/:id', (req, res) => {
+  axios.get(`http://3.138.228.145${req.url}`)
   .then((data) => res.send(data))
   .catch((err) => res.send(err));
 });
